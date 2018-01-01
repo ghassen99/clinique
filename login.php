6b417f24a39743775206bd225dc3eca88454b2c4
ghassen99 <ghassen.mallouli@iit.ens.tn>
2018-01-01 15:12:51 +0100

    essai de gestion des erreurs '400', ..

--- login.php
+++ login.php
@@ -13,16 +13,17 @@
         $action=$_REQUEST['action'];
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Authentification</title>
+    <title>Gentelella Alela! | </title>
 
     <!-- Bootstrap -->
     <link href="asset/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -30,84 +31,18 @@
     <link href="asset/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- NProgress -->
     <link href="asset/vendors/nprogress/nprogress.css" rel="stylesheet">
-    <!-- Datatables -->
-    <link href="asset/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-    <link href="asset/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
-    <link href="asset/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
-    <link href="asset/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
-    <link href="asset/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="asset/vendors/animate.css/animate.min.css" rel="stylesheet">
+
     <!-- Custom Theme Style -->
     <link href="asset/build/css/custom.min.css" rel="stylesheet">
+  </head>
 
-</head>
+  <body class="login">
+    <?php
+        include "controllers/".$controller.".controller.php";
+    ?>  
 
-<body>
-    <div class="container body">
-    <div class="main_container">
-
-        <!-- page content -->
-        <div class="right_col" role="main">
-        <div class="">
-            <div class="page-title">
-            </div>
-
-            <div>
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <div class="x_panel">
-                        <div>
-                            <?php
-                                include "controllers/".$controller.".controller.php";
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-        <!-- /page content -->
-
-        <!-- footer content -->
-        <footer>
-        <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-        </div>
-        <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-    </div>
-    </div>
-
-    <!-- jQuery -->
-    <script src="asset/vendors/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="asset/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- FastClick -->
-    <script src="asset/vendors/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="asset/vendors/nprogress/nprogress.js"></script>
-    
-    <!-- Custom Theme Scripts -->
-    <script src="asset/build/js/custom.min.js"></script>
-
-    <!-- validator -->
-    <script src="asset/vendors/validator/validator.js"></script>
-
-    <!-- Datatables -->
-    <script src="asset/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="asset/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-    <script src="asset/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="asset/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-    <script src="asset/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
-    <script src="asset/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="asset/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="asset/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-    <script src="asset/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-    <script src="asset/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="asset/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-    <script src="asset/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
-    <script src="asset/vendors/jszip/dist/jszip.min.js"></script>
-    <script src="asset/vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="asset/vendors/pdfmake/build/vfs_fonts.js"></script>
-
-</body>
+  </body>
 </html>
+
--- views/authentification/login.view.php
+++ views/authentification/login.view.php
@@ -1,44 +1,45 @@
-<div class="page-title">
-    <span style="text-align:center;"><h1>Authentification</h1></span><br>
-</div>
 
-<div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-    <div class="x_panel">
+        
 
-        <div class="x_content">
+                        
 
-        <form  method="post" action="login.php?controllers=login&action=login" class="form-horizontal form-label-left" novalidate >
-            
-                <!-- Login -->
-                <div class="item form-group">
-                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
-                    Login <span class="required">*</span>
-                    </label>
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <input id="login" class="form-control col-md-7 col-xs-12" data-validate-length-range="30" name="login" placeholder="Login" required="required" type="text">
-                    </div>
-                </div>
-            
-                <!-- password -->
-                <div class="item form-group">
-                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
-                    password <span class="required">*</span>
-                    </label>
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <input id="pass" type="password" class="form-control" data-validate-length-range="30" name="pass" placeholder="password" required="required">
-                    </div>
-                </div>
-            
-                <div class="ln_solid"></div>
-                <div class="form-group">
-                <div style="text-align:right;" class="col-md-6 col-md-offset-3">
-                    <button id="send" type="submit" class="btn btn-success">Submit</button>
-                    <button type="reset" class="btn btn-primary">Cancel</button>
-                </div>
-                </div>
-        </form>
+                        
+
+                        <div>
+<a class="hiddenanchor" id="signup"></a>
+<a class="hiddenanchor" id="signin"></a>
+
+<div class="login_wrapper">
+  <div class="animate form login_form">
+
+    <section class="login_content">
+
+    <form  method="post" action="login.php?controllers=login&action=login" class="form-horizontal form-label-left" novalidate >
+
+        <h1>Authentification</h1>
+        <div>
+        <input id="login" class="form-control col-md-7 col-xs-12" data-validate-length-range="30" name="login" placeholder="Login" required="required" type="text">
         </div>
-    </div>
-    </div>
+        <div>
+        <input id="pass" type="password" class="form-control" data-validate-length-range="30" name="pass" placeholder="password" required="required">
+        </div>
+        <div>
+            <button id="send" type="submit" class="btn btn-success">Submit</button>
+            <a class="reset_pass" href="#">Lost your password?</a>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="separator">
+          <div>
+            <p>©2017 All Rights Reserved. Ghassen MALLOULI</p>
+          </div>
+        </div>
+      </form>
+    </section>
 </div>
+
+</div>
+</div>
+
+
